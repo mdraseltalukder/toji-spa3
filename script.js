4a3acfb247a810707b235fefef1a703a3bfdbede
Md Rasel <mdrasel551219@gmail.com>
2025-05-28 12:58:56 +0600

    Add: Amination Added

--- script.js
+++ script.js
@@ -1,63 +1,473 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const carouselContainer = document.querySelector(".carousel-container");
-  const testimonialGroups = document.querySelectorAll(".testimonial-group");
-  const indicators = document.querySelectorAll(".indicator");
+// animation for the carousel
+gsap.registerPlugin(ScrollTrigger);
 
-  const totalItems = testimonialGroups.length;
-  let currentIndex = 0;
-  const autoplayInterval = 5000;
-  let autoplayTimer;
+const tl = gsap.timeline();
 
-  // Dynamic container width
-  carouselContainer.style.width = `${totalItems * 100}%`;
-
-  // Each item takes equal width
-  testimonialGroups.forEach((item) => {
-    item.style.width = `${100 / totalItems}%`;
-  });
-
-  function showSlide(index) {
-    currentIndex = index;
-
-    if (currentIndex >= totalItems) {
-      currentIndex = 0;
-    } else if (currentIndex < 0) {
-      currentIndex = totalItems - 1;
-    }
-
-    const translateValue = -currentIndex * (100 / totalItems) + "%";
-    carouselContainer.style.transform = `translateX(${translateValue})`;
-
-    indicators.forEach((indicator, i) => {
-      indicator.classList.toggle("active", i === currentIndex);
-    });
+tl.fromTo(
+  ".header-img",
+  {
+    opacity: 0,
+  },
+  {
+    opacity: 1,
+    scale: 1,
+    delay: 0.4,
+    duration: 0.4,
+    ease: "power2.out",
   }
+);
 
-  function startAutoplay() {
-    autoplayTimer = setInterval(() => {
-      showSlide(currentIndex + 1);
-    }, autoplayInterval);
+tl.fromTo(
+  ".desktob-nav",
+  {
+    opacity: 0,
+    y: 30,
+  },
+  {
+    opacity: 1,
+    y: 0,
+    duration: 0.6,
+    ease: "power2.out",
   }
+);
+// tl.fromTo(
+//   ".logo-img",
+//   {
+//     opacity: 0,
+//     scale: 1,
+//   },
+//   {
+//     scale: 1,
+//     opacity: 1,
+//     duration: 0.6,
+//     ease: "power2.out",
+//   }
+// );
 
-  function stopAutoplay() {
-    clearInterval(autoplayTimer);
+tl.fromTo(
+  "#rock_left",
+  {
+    opacity: 0,
+    x: -600,
+    rotate: -100,
+  },
+  {
+    opacity: 1,
+    x: 0,
+    rotate: 0,
+    duration: 1.6,
+    ease: "power2.out",
+  },
+  "rockStart"
+);
+
+tl.fromTo(
+  "#rock_right",
+  {
+    opacity: 0,
+    x: 600,
+    rotate: 100,
+  },
+  {
+    opacity: 1,
+    x: 0,
+    rotate: 0,
+    duration: 1.6,
+    ease: "power2.out",
+  },
+  "rockStart"
+);
+
+// scroll animation
+gsap.fromTo(
+  "#rock_right",
+  {
+    x: 0,
+    rotate: 0,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#rock_right",
+      start: "top 10%",
+      end: "bottom 20%",
+      scrub: true,
+    },
+    x: 600,
+    rotate: 100,
+    ease: "none",
   }
-
-  startAutoplay();
-
-  indicators.forEach((indicator) => {
-    indicator.addEventListener("click", () => {
-      const index = parseInt(indicator.getAttribute("data-index"));
-      showSlide(index);
-      stopAutoplay();
-      startAutoplay();
-    });
-  });
-
-  document
-    .querySelector(".testimonials-carousel")
-    .addEventListener("mouseenter", stopAutoplay);
-  document
-    .querySelector(".testimonials-carousel")
-    .addEventListener("mouseleave", startAutoplay);
+);
+gsap.fromTo(
+  "#rock_left",
+  {
+    x: 0,
+    rotate: 0,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#rock_right",
+      start: "top 10%",
+      end: "bottom 20%",
+      scrub: true,
+    },
+    x: -400,
+    rotate: -100,
+    ease: "none",
+  }
+);
+gsap.fromTo(
+  ".logo-img",
+  {
+    scale: 1,
+    opacity: 1,
+  },
+  {
+    scrollTrigger: {
+      trigger: ".logo-img",
+      start: "top 20%",
+      end: "bottom 20%",
+      scrub: true,
+    },
+    scale: 0.5,
+    opacity: 0.5,
+    ease: "none",
+  }
+);
+gsap.fromTo(
+  "#hero-title",
+  {
+    y: 100,
+    opacity: 0.3,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#hero-title",
+      start: "top 90%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+    y: 0,
+    opacity: 1,
+    ease: "none",
+  }
+);
+// about
+// about section bg animation
+gsap.to(".bg-animate", {
+  rotation: 5,
+  x: 50,
+  scale: 1.05,
+  duration: 6,
+  ease: "sine.inOut",
+  yoyo: true,
+  repeat: -1,
 });
+
+gsap.fromTo(
+  "#about-title",
+  {
+    filter: "blur(80px)",
+    y: 100,
+    scale: 0.4,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#about-title",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+
+    filter: "blur(0px)",
+    y: 0,
+    scale: 1,
+    ease: "power2.out",
+  }
+);
+gsap.to(".tree_left", {
+  scrollTrigger: {
+    trigger: ".tree_left",
+    start: "top 60%",
+    end: "bottom 20%",
+    scrub: true,
+  },
+
+  rotate: -30,
+  ease: "power2.inOut",
+});
+
+// service
+gsap.fromTo(
+  "#service-title",
+  {
+    filter: "blur(80px)",
+    y: 100,
+    scale: 0.4,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#service-title",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+
+    filter: "blur(0px)",
+    y: 0,
+    scale: 1,
+    ease: "power2.out",
+  }
+);
+// benefit
+gsap.fromTo(
+  "#benefit-title",
+  {
+    filter: "blur(80px)",
+    y: 100,
+    scale: 0.4,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#benefit-title",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+
+    filter: "blur(0px)",
+    y: 0,
+    ease: "power2.out",
+    scale: 1,
+  }
+);
+gsap.fromTo(
+  "#benefit-img",
+  {
+    rotate: -40,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#benefit-img",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+    rotate: 0,
+    ease: "power2.out",
+  }
+);
+gsap.fromTo(
+  "#benefit-content",
+  {
+    opacity: 0.2,
+    filter: "blur(10px)",
+  },
+  {
+    scrollTrigger: {
+      trigger: "#benefit-content",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+
+    opacity: 1,
+    filter: "blur(0px)",
+    ease: "power2.out",
+  }
+);
+gsap.to(".right-benefit-img", {
+  scrollTrigger: {
+    trigger: ".right-benefit-img",
+    start: "top 80%",
+    end: "bottom 40%",
+    scrub: true,
+  },
+
+  rotate: 0,
+  ease: "power2.inOut",
+});
+// bath
+
+gsap.fromTo(
+  "#bath-img",
+  {
+    rotate: -40,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#bath-img",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+    rotate: 0,
+    ease: "power2.out",
+  }
+);
+gsap.fromTo(
+  "#bath-content",
+  {
+    opacity: 0.2,
+    filter: "blur(10px)",
+  },
+  {
+    scrollTrigger: {
+      trigger: "#bath-content",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+
+    opacity: 1,
+    filter: "blur(0px)",
+    ease: "power2.out",
+  }
+);
+// hotstone
+
+gsap.fromTo(
+  "#hotstone-img",
+  {
+    rotate: 40,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#hotstone-img",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+    rotate: 0,
+    ease: "power2.out",
+  }
+);
+gsap.fromTo(
+  "#hotstone-content",
+  {
+    opacity: 0.2,
+    filter: "blur(10px)",
+  },
+  {
+    scrollTrigger: {
+      trigger: "#hotstone-content",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+
+    opacity: 1,
+    filter: "blur(0px)",
+    ease: "power2.out",
+  }
+);
+
+// testimonial
+
+gsap.fromTo(
+  "#testimonial-title",
+  {
+    filter: "blur(80px)",
+    y: 100,
+    scale: 0.8,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#testimonial-title",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+
+    filter: "blur(0px)",
+    y: 0,
+    ease: "power2.out",
+    scale: 1,
+  }
+);
+gsap.fromTo(
+  ".right-tree",
+  {
+    scale0: 0.8,
+  },
+  {
+    scrollTrigger: {
+      trigger: ".right-tree",
+      start: "top 60%",
+      end: "bottom 20%",
+      scrub: true,
+    },
+    scale: 1,
+    rotate: 30,
+    ease: "power2.inOut",
+  }
+);
+
+// career
+
+gsap.fromTo(
+  "#career-title",
+  {
+    filter: "blur(80px)",
+    y: 100,
+    scale: 0.8,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#career-title",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+
+    filter: "blur(0px)",
+    y: 0,
+    ease: "power2.out",
+    scale: 1,
+  }
+);
+// training
+
+gsap.fromTo(
+  "#training-title",
+  {
+    filter: "blur(80px)",
+    y: 100,
+    scale: 0.8,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#training-title",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+
+    filter: "blur(0px)",
+    y: 0,
+    ease: "power2.out",
+    scale: 1,
+  }
+);
+// expertise
+
+gsap.fromTo(
+  "#expertise-title",
+  {
+    filter: "blur(80px)",
+    y: 100,
+    scale: 0.8,
+  },
+  {
+    scrollTrigger: {
+      trigger: "#expertise-title",
+      start: "top 100%",
+      end: "bottom 60%",
+      scrub: true,
+    },
+
+    filter: "blur(0px)",
+    y: 0,
+    ease: "power2.out",
+    scale: 1,
+  }
+);
